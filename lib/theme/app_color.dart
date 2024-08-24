d931ff809c26bdbff4dcf50eccec7e7b301da5d3
RanaAbowaeda <rana.abowarda.94@gmail.com>
2024-08-24 15:41:04 +0300

    Start project. Theme finished. Empty home screen.

---
+++ lib/theme/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Color(0xff39A552);
+  static const Color secondaryColor = Color(0xff4F5A69);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color blackColor = Color(0xff303030);
+  static const Color redColor = Color(0xffC91C22);
+  static const Color blueColor = Color(0xff4882CF);
+  static const Color pinkColor = Color(0xffED1E79);
+  static const Color brownColor = Color(0xffCF7E48);
+  static const Color seconedBlueColor = Color(0xff003E90);
+  static const Color yelloyColor = Color(0xffF2D352);
+}
